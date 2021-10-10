03b4a176a4c218d6182311c8aa776fd26fdf54cf
Yusufbek Orzibekov <75078482+yusuforzibekov@users.noreply.github.com>
2021-10-10 11:41:34 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Inputda kiritilgan sonlardan qay biri katta ekanligini topib beruvchi dastur")
+a = int(input("a sonni kiriting: "))
+b = int(input("b sonni kiriting: "))
+if a > b:
+    print("a son katta -", a)
+else:
+    print("b son katta -", b)
